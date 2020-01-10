b651fa6fa58d5943958adc35c89b78651e950d59
zhaohq <610006480@qq.com>
2020-01-10 16:43:32 +0800

    11

--- spring-boot-samples-monitoring-prometheus-grafana/src/main/java/io/github/y0ngb1n/monitoring/prometheusgrafana/IndexController.java
+++ spring-boot-samples-monitoring-prometheus-grafana/src/main/java/io/github/y0ngb1n/monitoring/prometheusgrafana/IndexController.java
@@ -1,7 +1,10 @@
 package io.github.y0ngb1n.monitoring.prometheusgrafana;
 
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +16,26 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/index")
 public class IndexController {
 
-  static final Counter requestCounter = Counter.build()
+  @Autowired
+  private CollectorRegistry collectorRegistry;
+    /*final Counter requestCounter = Counter.build()
     .name("io_namespace_http_requests_total").labelNames("path", "method")
-    .help("Total requests.").register();
+    .help("Total requests.").register(collectorRegistry);*/
+
+    @Autowired
+  private Counter requestCounter;
+
+
+
+
+ // @Autowired
+  Counter counter;
 
   @GetMapping
   public void index(HttpServletRequest request) {
     log.info("aa");
     requestCounter.labels(request.getRequestURI(), request.getMethod()).inc();
+  //  counter.labels(request.getRequestURI(), request.getMethod(),200+"").inc();
   }
 
 }
--- spring-boot-samples-monitoring-prometheus-grafana/src/main/java/io/github/y0ngb1n/monitoring/prometheusgrafana/PrometheusGrafanaApplication.java
+++ spring-boot-samples-monitoring-prometheus-grafana/src/main/java/io/github/y0ngb1n/monitoring/prometheusgrafana/PrometheusGrafanaApplication.java
@@ -1,7 +1,11 @@
 package io.github.y0ngb1n.monitoring.prometheusgrafana;
 
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 使用 Prometheus & Grafana 监控你的 Spring Boot 应用.
@@ -14,4 +18,27 @@ public class PrometheusGrafanaApplication {
   public static void main(String[] args) {
     SpringApplication.run(PrometheusGrafanaApplication.class, args);
   }
+
+  @Autowired
+  private CollectorRegistry collectorRegistry;
+
+  @Bean
+  public Counter requestTotalCountCollector(){
+   /* Counter requestCounter = Counter.build()
+      .name("io_namespace_http_requests_total").labelNames("path", "method")
+      .help("Total requests.").register();*/
+
+    return  Counter.build()
+      .name("http_requests_total")
+      .labelNames("path", "method", "code")
+      .help("http请求总计数").register(collectorRegistry);
+  }
+
+  @Bean
+  public Counter requestCounter(){
+    return Counter.build()
+      .name("io_namespace_http_requests_total").labelNames("path", "method")
+      .help("Total requests.").register(collectorRegistry );
+  }
+
 }
