5c69949e9625c12c072856a4822bb5ccfed2c898
zhaohq <610006480@qq.com>
2020-03-19 15:17:04 +0800

    11

--- spring-boot-samples-monitoring-prometheus-grafana/src/main/java/io/github/y0ngb1n/monitoring/prometheusgrafana/IndexController.java
+++ spring-boot-samples-monitoring-prometheus-grafana/src/main/java/io/github/y0ngb1n/monitoring/prometheusgrafana/IndexController.java
@@ -2,6 +2,7 @@ package io.github.y0ngb1n.monitoring.prometheusgrafana;
 
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.PushGateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -31,8 +33,27 @@ public class IndexController {
  // @Autowired
   Counter counter;
 
+  public static final Counter counterDemo = Counter.build()
+    .name("push_way_counter")
+    .labelNames("code", "instance")
+    .help("user-service异常统计")
+    .register();
+
+
+
   @GetMapping
   public void index(HttpServletRequest request) {
+    //统计异常
+    PushGateway prometheusPush = new PushGateway("47.97.158.62:9091");
+    //指标值增加
+    counterDemo.labels(222+"", "botaa").inc();
+    try {
+      prometheusPush.push(counterDemo, "ex-user-service");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
     log.info("aa");
     requestCounter.labels(request.getRequestURI(), request.getMethod()).inc();
   //  counter.labels(request.getRequestURI(), request.getMethod(),200+"").inc();
